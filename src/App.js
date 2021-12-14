d3b1340bfc73ba8202d445cec9b66024a2144a61
samsujjoha313 <samsujjoha313@gmail.com>
2021-12-14 10:55:06 +0600

    added form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from 'react'
-import {Routes,Route,Navigate} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Search from './components/Search';
-import Result from './components/Result';
+import SignUp from './components/FormSignup'
 
-
-
-function App() {
-    return (
-        <>
-        <Navbar/>
-        
-        <Routes>
-        <Route path="/" element={<Navigate to ="/home"/>}/>
-        <Route path="/home/*" element={<Home/>}/>
-        <Route path="world" element={<p>This is the sub home</p>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/search" element={<Search/>}/>
-        <Route path="/result" element={<Result/>}/>
-        
-        </Routes>
-        
-        
-        
-            
-        </>
-    )
+const App = () => {
+  return (
+    <div>
+    <SignUp/>  
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/useForm.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const useForm = (validate) => {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+  const [errors, setErrors] = useState({});
+  
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setErrors(validate(values));
+   
+  };
+
+
+
+  return { handleChange, handleSubmit, values, errors };
+};
+
+export default useForm;
